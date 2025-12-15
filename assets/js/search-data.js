e78f0898f059a8783ab7fb21a85542039360974b
mijinkoo <79077316+mijinkoo@users.noreply.github.com>
2025-12-15 08:14:00 +0000

    Deploying to gh-pages from @ mijinkoo/mijinkoo.github.io@a56ed7d0c6e37793f8fbd4861c68f6597138a112 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
